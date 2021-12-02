652080a0e2a2fc2c5f74a22fd0f6e154be360fd4
Deusald <deusald@gmail.com>
2021-12-03 00:18:10 +0100

    version(DeusaldSharp): updated plugin to version 5.0.3

--- DeusaldSharp/DeusaldSharp.cs
+++ DeusaldSharp/DeusaldSharp.cs
@@ -27,6 +27,6 @@ namespace DeusaldSharp
 {
     public static class DeusaldSharp
     {
-        public static readonly Version Version = new Version(5, 0, 2);
+        public static readonly Version Version = new Version(5, 0, 3);
     }
 }
