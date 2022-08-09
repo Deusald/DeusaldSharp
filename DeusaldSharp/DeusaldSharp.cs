6dacfd870ad8595ec9dd6021eaced869f5c6bace
Deusald <deusald@gmail.com>
2022-08-09 18:42:41 +0200

    version(DeusaldSharp): updated library to version 5.3.0

--- DeusaldSharp/DeusaldSharp.cs
+++ DeusaldSharp/DeusaldSharp.cs
@@ -27,6 +27,6 @@ namespace DeusaldSharp
 {
     public static class DeusaldSharp
     {
-        public static readonly Version Version = new Version(5, 2, 4);
+        public static readonly Version Version = new Version(5, 3, 0);
     }
 }
